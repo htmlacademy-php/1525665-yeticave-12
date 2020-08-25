551f692cbdb5848731825880929272c7cbfc6dc5
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-08-25 17:33:46 +0300

    Первый commit
    Ошибок не выдает. Новый лот, добавленный вручную, успешно появляется первым в списке на главной страничке. У каждой категории своя подходящая картинка.

--- main.php
+++ main.php
@@ -6,8 +6,8 @@
             <?php
                 foreach($categories as $category):
                ?>
-            <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
+            <li class="promo__item promo__item--<?=$category['img']; ?>">
+                    <a class="promo__link" href="pages/all-lots.html"><?=$category['name']; ?></a>
               </li>
               <?php endforeach; ?>
         </ul>
@@ -17,26 +17,26 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($products as $product): $result = deletion_of_lot($product['date']); ?>
+            <?php foreach ($products as $product): $result_time = deletion_of_lot($product['date_delection']); ?>
             <li class="lots__item lot">
               <div class="image">
-                    <img src="<?=$product['image']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$product['url']; ?>" width="350" height="260" alt="">
               </div>
               <div class="lot__info">
-                 <span class="lot__category"><?=$product['category'];?></span>
-                  <h3 class="lot__title">  <a class="text-link" href="pages/lot.html"><?= htmlspecialchars($product['title']);?></a></h3>
+                 <span class="lot__category"><?=$product['category_name'];?></span>
+                  <h3 class="lot__title">  <a class="text-link" href="pages/lot.html"><?= htmlspecialchars($product['name']);?></a></h3>
                   <div class="lot__state">
                       <div class="lot__rate">
                           <span class="lot__amount">Стартовая цена</span>
-                         <span class="lot__cost"> <?= htmlspecialchars(format_sum($product['cost'])); ?></span>
+                         <span class="lot__cost"> <?= htmlspecialchars(format_sum($product['first_price'])); ?></span>
                       </div>
                       <div class="lot__timer timer <?php
-                      if ($result[0] < 1){
+                      if ($result_time[0] < 1){
                      echo ' timer--finishing';
                    };
                        ?>"
                        >
-                          <?=  $rest_time = implode(":", $result);
+                          <?=  $rest_time = implode(":", $result_time);
                           ?>
                       </div>
                     </div>
