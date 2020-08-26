bd067a942c702d711cda3ad7c99271c6bf6a4ceb
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-08-26 09:47:21 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,25 +3,19 @@
   if ($con == false) {
     exit;
   }
-  else {
     mysqli_set_charset($con, "utf8");
     $sql_cat = "SELECT name, id, img FROM categories;";
     $result_cat = mysqli_query($con, $sql_cat);
     if (!$result_cat) {
       exit;
     }
-    else{
-      $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
-
-    }
+    $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
     $sql_lots = "SELECT  categories.name AS category_name, lots.name, first_price, url, date_delection, bet_step FROM lots JOIN categories ON categories.id = lots.category_id WHERE date_delection > NOW() ORDER BY date_delection DESC;";
     $result_lots = mysqli_query($con, $sql_lots);
     if (!$result_lots) {
-      $error = mysqli_error($con);
-      print("Ошибка MySQL: " . $error);
+      exit;
     }
     $products = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
-  }
 
       require_once("function.php");
       require_once("helpers.php");
