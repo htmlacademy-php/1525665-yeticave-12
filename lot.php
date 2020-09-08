d1f09918f2d6d93250de8a5e5b5cb3bbaa273eec
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-09-08 16:04:23 +0300

    Best commit

--- lot.php
+++ lot.php
@@ -1,32 +1,31 @@
 <?php
-$id = $_GET['id'] ?? NULL;
-if ($id === NULL) {
-  header("Location: pages/404.html");
-}
-    require_once("./function.php");
-    require_once("init.php");
-    require_once("helpers.php");
-    $is_auth = rand(0, 1);
     $id = $_GET['id'] ?? NULL;
     if ($id === NULL) {
       header("Location: pages/404.html");
+      exit;
     }
+    require_once("./function.php");
+    require_once("init.php");
+    require_once("helpers.php");
+    $sql_lot = "SELECT categories.name AS category_name, lots.name, lots.id AS id, description, first_price, url, date_delection, bet_step FROM lots JOIN categories ON categories.id = lots.category_id WHERE lots.id = $id;";
+    $result_lot = mysqli_query($con, $sql_lot);
+    if (!$result_lot) {
+      exit;
+    }
+    $lot_info = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
+    $lot = $lot_info[0];
 
-      // Получаю всю информацию о лоте
-      $sql_lot = "SELECT categories.name AS category_name, lots.name, lots.id, description, first_price, url, date_delection, bet_step FROM lots JOIN categories ON categories.id = lots.category_id WHERE lots.id = $id;";
-      $result_lot = mysqli_query($con, $sql_lot);
-      if ($result_lot === NULL) {
-        header("Location: /pages/404.html");
-      }
-      $lot_info = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
-      $lot = $lot_info[0];
-
+    $is_auth = rand(0, 1);
+    if ($id != $lot['id']) {
+      header("Location: pages/404.html");
+      exit;
+    }
       $result_time = deletion_of_lot($lot['date_delection']);
       // Получаю максимальную ставку для того, чтобы сложить ее с первоначальной ценой
       $sql_current = "SELECT MAX(cost) AS cost, lot_id FROM bets JOIN lots ON lots.id = bets.lot_id WHERE lots.id = $id;";
       $result_current = mysqli_query($con, $sql_current);
       if (!$result_current) {
-        header("Location: /pages/404.html");
+       exit;
       }
       $lot_max_bet = mysqli_fetch_assoc($result_current);
       $lmb = $lot_max_bet['cost'];
