7233843698ca16dfee3f90906504116650cf0023
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-09-10 12:07:27 +0300

    Изменил ссылку на страницу лота

--- templates/main.php
+++ templates/main.php
@@ -27,10 +27,10 @@
               </div>
               <div class="lot__info">
                 <?php
-                $lot_url = "?id=" . http_build_query($product);
+                $lot_url = 'lot.php?' . http_build_query(['id' => $product['id'] ]);
                 ?>
                  <span class="lot__category"><?=$product['category_name'];?></span>
-                  <h3 class="lot__title">  <a class="text-link" href="lot.php<?= $lot_url; ?>"><?= htmlspecialchars($product['name']);?></a></h3>
+                  <h3 class="lot__title">  <a class="text-link" href="<?= $lot_url; ?>"><?= htmlspecialchars($product['name']);?></a></h3>
                   <div class="lot__state">
                       <div class="lot__rate">
                           <span class="lot__amount">Стартовая цена</span>
