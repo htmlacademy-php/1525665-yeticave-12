9a0fb307e53c06ec66324a134ec042cff1ee6460
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-09-23 23:25:19 +0300

    Идентичный коммит

--- add.php
+++ add.php
@@ -1,62 +1,48 @@
-<main>
-<nav class="nav">
-    <ul class="nav__list container">
-      <?php foreach ($categories as $cat): ?>
-      <li class="nav__item">
-        <a href="all-lots.html"><?= $cat['name']; ?></a>
-      </li>
-    <?php endforeach; ?>
-    </ul>
-  </nav>
-  <form class="form form--add-lot container" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
-    <h2>Добавление лота</h2>
-    <div class="form__container-two">
-      <div class="form__item"> <!-- form__item--invalid -->
-        <label for="lot-name">Наименование <sup>*</sup></label>
-        <input id="lot-name" type="text" name="title" placeholder="Введите наименование лота" value="<?=getPostVal('title'); ?>">
+<?php
+      require_once("function.php");
+      require_once("init.php");
+      require_once("helpers.php");
+  //
+  $is_auth = rand(0, 1);
+  $errors = [];
+  $rules = [
+    'title' => function() {
+        return validateFilled('title');
+    },
+    'first_price' => function() {
+        return validateFilled('first_price');
+    },
+    'description' => function() {
+        return validateFilled('description');
+    },
+    'bet_step' => function() {
+        return validateFilled('bet_step');
+    },
+    'date_delection' => function() {
+        return is_date_valid('date_delection');
+    }
+];
 
-      </div>
-      <div class="form__item">
-        <label for="category">Категория <sup>*</sup></label>
-        <select id="category" name="category">
-          <?php foreach($categories as $category): ?>
-          <option><?= $category['name']; ?></option>
-            <?php endforeach; ?>
-        </select>
-        <span class="form__error">Выберите категорию</span>
-      </div>
-    </div>
-    <div class="form__item form__item--wide">
-      <label for="message">Описание <sup>*</sup></label>
-      <textarea id="message" name="description" placeholder="Напишите описание лота" value="<?=getPostVal('description'); ?>"></textarea>
-      <span class="form__error">Напишите описание лота</span>
-    </div>
-    <div class="form__item form__item--file">
-      <label>Изображение <sup>*</sup></label>
-      <div class="form__input-file">
-        <input class="visually-hidden" type="file" id="lot-img" value="<?=getPostVal('lot-img'); ?>">
-        <label for="lot-img">
-          Добавить
-        </label>
-      </div>
-    </div>
-    <div class="form__container-three">
-      <div class="form__item form__item--small">
-        <label for="lot-rate">Начальная цена <sup>*</sup></label>
-        <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?=getPostVal('lot-rate'); ?>">
-        <span class="form__error">Введите начальную цену</span>
-      </div>
-      <div class="form__item form__item--small">
-        <label for="lot-step">Шаг ставки <sup>*</sup></label>
-        <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?=getPostVal('lot-step'); ?>">
-        <span class="form__error">Введите шаг ставки</span>
-      </div>
-      <div class="form__item">
-        <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-        <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?=getPostVal('lot-date'); ?>">
-        <span class="form__error">Введите дату завершения торгов</span>
-      </div>
-    </div>
-    <button type="submit" class="button">Добавить лот</button>
-  </form>
-</main>
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $lot = $_POST;
+
+        $filename = uniqid() . $file;
+        $lot['foto'] = $filename;
+        move_uploaded_file($_FILES['']['t'], 'uploads/' . $filename);
+
+        $sql = 'INSERT INTO lots (date_creation, name, category_id, description, foto, date_delection) VALUES (NOW(), ?, ?, ?, ?, ?)';
+
+        $stmt = db_get_prepare_stmt($link, $sql, $lot);
+        $res = mysqli_stmt_execute($stmt);
+        if ($res) {
+            $lot_id = mysqli_insert_id($link);
+
+            header("Location: lot.php?id=" . $lot_id );
+        }
+}
+
+$errors = array_filter($errors);
+$content = include_template('add.php', ['categories' => $categories, 'connection' => $connection, 'rules' => $rules, 'errors' => $errors]);
+$layout_content = include_template('layout.php', ['content' => $content, 'title' => 'Добавление лота', 'categories' => $categories, 'is_auth' => $is_auth, 'user_name' => 'Илья']);
+print($layout_content);
+?>
