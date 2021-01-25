76728585c17adf46d51019e33c4fe0646eb99658
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2021-01-25 21:36:53 +0300

    Написал сценарий регистрации
    Валидация полей работает успешно, email проверяется на оригинальность, в БД данные добавляются.

---
+++ sign-up.php
@@ -0,0 +1,69 @@
+<?php
+  require_once("init.php");
+  require_once("function.php");
+  require_once("helpers.php");
+  $is_auth = rand(0, 1);
+  $errors = [];
+  $user = $_POST;
+  $rules = [
+    'email' => function(){
+        if(!validateEmail('email')){
+          return "Введите корректный email";
+        }
+    },
+    'password' => function(){
+      if(!validateFilled('password')){
+        return "Заполните это поле";
+      }
+    },
+    'name' => function(){
+      if(!validateFilled('name')){
+        return "Заполните это поле";
+      }
+    },
+    'message' => function(){
+      if(!validateFilled('message')){
+        return "Заполните это поле";
+      }
+    }
+];
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    foreach ($user as $key => $value) {
+         if (isset($rules[$key])) {
+           $rule = $rules[$key];
+           $result = $rule($value);
+           if ($result !== null) {
+              $errors[$key] = $result;
+            }
+         }
+     }
+  }
+  if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($errors)) {
+         $user = $_POST;
+         $safe_email = mysqli_real_escape_string($connection, $user['email']);
+         $sql = "SELECT id FROM users WHERE email = '$safe_email'";
+         $res = mysqli_query($connection, $sql);
+         if (mysqli_num_rows($res) > 0) {
+           $errors['email'] = 'Пользователь с этим email уже зарегистрирован';
+         }
+         else{
+              $safe_name = mysqli_real_escape_string($connection, $_POST['name']);
+              $safe_message = mysqli_real_escape_string($connection, $_POST['message']);
+              $passwordHash = password_hash($user['password'], PASSWORD_DEFAULT);
+              $new_user = [$safe_name, $safe_email, $safe_message, $passwordHash];
+              $sql = 'INSERT INTO users (register_date, name, email, contacts, password) VALUES (NOW(), ?, ?, ?, ?)';
+              $stmt = db_get_prepare_stmt($connection, $sql, $new_user);
+              $res = mysqli_stmt_execute($stmt);
+
+              if ($res && empty($errors)) {
+                header("Location: pages/login.html");
+                exit;
+              }
+          }
+    }
+
+  $content = include_template('sign-up.php', ['categories' => $categories, 'connection' => $connection, 'rules' => $rules, 'errors' => $errors]);
+  $layout_content = include_template('layout.php', ['content' => $content, 'title' => 'Регистрация', 'categories' => $categories, 'is_auth' => $is_auth, 'user_name' => 'Илья', 'rules' => $rules]);
+  print($layout_content);
+?>
