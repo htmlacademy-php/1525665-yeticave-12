f17aa5ff98224dde7bc7784f70e61f350e4638d2
Ilya <iperederin@icloud.com>
2021-03-22 17:24:31 +0300

    Настроил "логику" сессий

--- init.php
+++ init.php
@@ -1,5 +1,14 @@
 <?php
   session_start();
+  if(isset($_SESSION['user_id']) && $_SESSION['user_name']){
+      $user_id = $_SESSION['user_id'];
+      $is_auth = 1;
+      $username = $_SESSION['user_name'];
+  }
+  else{
+      $is_auth = 0;
+      $username = null;
+  }
   $connection = mysqli_connect("localhost", "root", "", "yeticave");
   if ($connection === false) {
     exit;
